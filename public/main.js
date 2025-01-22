e22af9698d6713877be37ef4c815b4df4a14763e
Pranav Bhosale <pranavbhosalebhosale@gmail.com>
2025-01-22 19:32:02 +0530

    Validform commit with image extension

--- public/main.js
+++ public/main.js
@@ -1,18 +1,44 @@
-const select = (selector) => { return documet.querySelector(selector);
+const select = (selector) => {
+  return document.querySelector(selector);
+};
+const form = select(".form");
+const Message = select(".message");
+
+const DisplayMessage = (text, color) => {
+  Message.style.visibility = "visible";
+  Message.style.backgroundColor = color;
+  Message.innerText = text;
+
+  setTimeout(()=>{
+    Message.style.visibility="hidden"
+  },3000)
+
 };
 
+const validateForm = () => {
+  const title = select("#title").value.trim();
+  const content = select("#content").value.trim();
+  const thumbnail = select("#thumbnail").value;
+  const category = select("#category").value;
 
-const validateForm=()=>{
+  const exceptedImageFiles = ['jpg','png','jpeg']
 
-  const title = select('#title').value();
+  if (!title || !content || !thumbnail || category == "0") {
+    //Show Error in Display msg
+    return DisplayMessage("Field Cannot be Emplty", "red");
+  }
   
+  const extension = thumbnail.split('.').pop();
+  if(!exceptedImageFiles.includes(extension))
+    {
+ return DisplayMessage("Image Fille is Not Valid", "red");
 
-return true;
-}
+    }
 
-const form = select('.form')
+  return true;
+};
 
-form.addEventLister('submit',(e)=>{
-  
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-})
+  validateForm();
+});

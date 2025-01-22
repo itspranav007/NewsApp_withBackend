96d6811b025117745a7d916d3c50e0bdbf125585
Pranav Bhosale <pranavbhosalebhosale@gmail.com>
2025-01-22 15:18:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+
+  res.send('<h1>Server</h1>')
+})
+
+
+const PORT =5000;
+
+app.listen(PORT,()=>{
+
+  console.log(`Server Running On Server http://localhost:${PORT}`)
+})

9f5b0976afc6f4e35348a59810023410e2cf11ab
Pranav Bhosale <pranavbhosalebhosale@gmail.com>
2025-01-23 13:16:21 +0530

    Image Resize using multer Add id before ImageName

--- routers/newsrouter.js
+++ routers/newsrouter.js
@@ -1,31 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
+const fs = require("fs");
 //==>Image Resize Before Saving
-const sharp = require('sharp');
-const multer = require('multer');
-const fs = require('fs')
+const News = require("../news/news");
+const sharp = require("sharp");
+const multer = require("multer");
+
 const storage = multer.memoryStorage();
-const upload = multer({storage})
+const upload = multer({ storage });
 
+router.post("/create", upload.single("thumbnail"), async (req, res) => {
+  res.send("Submit successful");
 
-router.post('/create',upload.single('thumbnail'),async(req,res)=>{
-  res.send('Submit successful');
+  console.log("file", req.file); //=>check
+  console.log("body", req.body); //==>check
 
-console.log('file',req.file)//=>check 
-console.log('body',req.body)//==>check
-
-fs.access('./data/uploadsimage',(err)=>{
-  if(err)
-    {
-      fs.mkdirSync('./data/uploadsimage')
+  fs.access("./data/uploadsimage/", (err) => {
+    if (err) {
+      fs.mkdirSync("./data/uploadsimage/");
     }
-})
-await sharp(req.file.buffer).resize({width:615,height:350}).toFile('./data/uploadsimage'+req.file.originalname);
+  });
 
+  const id = new News().createId();
+  const fileName = `${id}-${req.file.originalname}`
+  await sharp(req.file.buffer)
+    .resize({ width: 615, height: 350 })
+    .toFile(`./data/uploadsimage/${fileName}` + req.file.originalname);
+});
 
-
-})
-
-
-module.exports=router;
+module.exports = router;

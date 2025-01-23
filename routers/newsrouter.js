f9da04661f7e412e53509af914800edc5ba5e028
Pranav Bhosale <pranavbhosalebhosale@gmail.com>
2025-01-23 13:03:15 +0530

    Image Resize using multer and sharp ,uploads in file uploadsimage using fs , give height and width for image resolution

--- routers/newsrouter.js
+++ routers/newsrouter.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+//==>Image Resize Before Saving
+const sharp = require('sharp');
+const multer = require('multer');
+const fs = require('fs')
+const storage = multer.memoryStorage();
+const upload = multer({storage})
 
-router.post('/create',(req,res)=>{
 
-  res.send('Submit successful')
+router.post('/create',upload.single('thumbnail'),async(req,res)=>{
+  res.send('Submit successful');
+
+console.log('file',req.file)//=>check 
+console.log('body',req.body)//==>check
+
+fs.access('./data/uploadsimage',(err)=>{
+  if(err)
+    {
+      fs.mkdirSync('./data/uploadsimage')
+    }
+})
+await sharp(req.file.buffer).resize({width:615,height:350}).toFile('./data/uploadsimage'+req.file.originalname);
+
+
+
 })
 
 

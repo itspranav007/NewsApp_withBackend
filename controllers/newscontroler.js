d4c70a6a4cb10cb07f22adc171cf8092e4f6f6d7
Pranav Bhosale <pranavbhosalebhosale@gmail.com>
2025-01-23 15:42:25 +0530

    Expression validator

--- controllers/newscontroler.js
+++ controllers/newscontroler.js
@@ -1,22 +1,19 @@
-const News = require('../news/news');
+const News = require("../news/news");
 const ImageProcess = require("../utils/imageProcess");
 
 const createNews = async (req, res) => {
   const news = new News();
-  const id = news.createId(); // Assuming this method generates a unique ID for news
+  const id = news.createId();
 
   try {
-    const imageName = await ImageProcess(req, id); // Process the image and get its name
+    const imageName = await ImageProcess(req, id);
 
-    // Add news data (assumes `create` method takes body, ID, and image name)
     await news.create(req.body, id, imageName);
 
-    // Respond with a success message
     res.status(200).send("Submit successful");
   } catch (error) {
     console.error("Error While Creating News In Controller:", error);
 
-    // Respond with an error message
     res.status(500).send("Failed to create news.");
   }
 };
--- routers/newsrouter.js
+++ routers/newsrouter.js
@@ -5,7 +5,31 @@ const router = express.Router();
 const uploads = require("../middleware/multer");
 const NewsController = require("../controllers/newscontroler");
 
+const {check,validationResult} = require('express-validator');
+const expectedCategory = ['tech','political','education']
+const validator = [ 
+  check('title').trim().not().isEmpty().withMessage('Title is Required !'),
+  check('content').trim().not().isEmpty().withMessage('content is Required !'),
+  check('thumbnail').trim().not().isEmpty().withMessage('thumbnail is Required !'),
+  check('category').isIn(expectedCategory).withMessage('select category atleast One !'),
+]
+
+const result = (req,res,next)=>{
+  const result = validationResult(req)
+  const hasError =! result.isEmpty();
+
+  if(hasError){
+    const error = result.array()[0].msg
+    res.json({success:false,message:error})
+  }
+
+  next();
+}
+
+
 // Route for creating news
-router.post("/create", uploads.single("thumbnail"), NewsController.createNews);
+router.post("/create", uploads.single("thumbnail"),validator,result, NewsController.createNews);
+
+
 
 module.exports = router;
